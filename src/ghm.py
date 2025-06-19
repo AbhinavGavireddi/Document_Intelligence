8df296802fe9be9f7da592abca4919c062687c20
Abhinav Gavireddi <=>
2025-06-20 04:46:49 +0530

    [fix]: fixed pdf parsing

--- src/ghm.py
+++ src/ghm.py
@@ -59,8 +59,8 @@ def initialize_models():
     #     shutil.rmtree(user_paddleocr_dir)
     # shutil.copytree(paddleocr_model_dir, user_paddleocr_dir)
 
-    json_url = 'https://github.com/opendatalab/MinerU/raw/master/magic-pdf.template.json'
-    config_file_name = 'magic-pdf.json'
+    json_url = 'https://github.com/opendatalab/MinerU/raw/master/mineru.template.json'
+    config_file_name = 'mineru.json'
     home_dir = os.path.expanduser('~')
     config_file = os.path.join(home_dir, config_file_name)
 
